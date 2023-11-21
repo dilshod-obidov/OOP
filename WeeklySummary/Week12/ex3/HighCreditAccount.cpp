45a5acdc26b728700f2561cef458071322682f99
Dilshod <137488386+dilshod-obidov@users.noreply.github.com>
2023-11-21 07:13:20 +0000

    Week12 ex3

--- WeeklySummary/Week12/ex3/HighCreditAccount.cpp
+++ WeeklySummary/Week12/ex3/HighCreditAccount.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 void HighCreditAccount::Deposit(int money){
     if(money > 0){
-        Account::Deposit(money*(100+m_SpecialRate)*1.0/100);
+        Account::Deposit(money*(100+m_SpecialRate)/100);
     }
 }
--- WeeklySummary/Week12/ex3/SavingAccount.cpp
+++ WeeklySummary/Week12/ex3/SavingAccount.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 void SavingAccount::Deposit(int money){
     if(money > 0){
-        Account::Deposit(money*(100+m_InterestRate)*1.0/100);
+        Account::Deposit(money*(100+m_InterestRate)/100);
     }
 }
