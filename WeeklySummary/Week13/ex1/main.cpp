ebaf18827942f9b2884c20e13fef5876aa6ab7aa
Dilshod <137488386+dilshod-obidov@users.noreply.github.com>
2023-11-23 05:34:01 +0000

    Week13 ex1 complete

--- WeeklySummary/Week13/ex1/main.cpp
+++ WeeklySummary/Week13/ex1/main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 
+#define MIN(p, q) ((p > q) ? p : q)
+#define POSITIVE(n) ((n < 0) ? -n : n)
+
 using namespace std;
 
 class Fraction{
 private:
     int m_numerator;
     int m_denominator;
+    
 public:
-    Fraction(int numerator = 0, int denominator = 1) 
-        : m_numerator{numerator}, m_denominator{denominator} {}
+    Fraction(int numerator = 0, int denominator = 1){
+        // Negative values handler
+        if(denominator < 0){
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+        
+        // Simplifier
+        for(int i = MIN(POSITIVE(numerator), denominator); i > 1; i--){
+            if(numerator % i == 0 && denominator % i == 0){
+                numerator /= i;
+                denominator /= i;
+                break;
+            }
+        }
+
+        m_numerator = numerator;
+        m_denominator = denominator; 
+    }
     
     friend ostream& operator<<(ostream&, const Fraction&);
 };
