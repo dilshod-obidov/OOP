c1b1a240f76f79f3eda9a415763dd87842f7838d
Dilshod <137488386+dilshod-obidov@users.noreply.github.com>
2023-11-27 12:36:25 +0000

    subscript const

--- WeeklySummary/Week14/Operator-Overloading-2/subscript.cpp
+++ WeeklySummary/Week14/Operator-Overloading-2/subscript.cpp
@@ -9,17 +9,36 @@ private:
     int m_list[SIZE];
 
 public:
+    IntList(){
+        for(int i = 0; i < SIZE; ++i){
+            m_list[i] = i * 2;
+        }
+    }
+
+    // return a reference
     int& operator[](int);
+    // const operator[]
+    const int& operator[](int) const;
 };
 
 int& IntList::operator[](int index){
     return m_list[index];
 }
 
+const int& IntList::operator[](int index) const{
+    return m_list[index];
+}
+
 int main(){
     IntList list;
-    for(int i = 0; i < SIZE; ++i) list[i] = i * 3;
-
     for(int i = 0; i < SIZE; ++i) cout << list[i] << " ";
+    cout << endl;
+    list[0] = 100; // okay
+    for(int i = 0; i < SIZE; ++i) cout << list[i] << " ";
+    cout << endl;
+
+    const IntList c_list;
+    for(int i = 0; i < SIZE; ++i) cout << c_list[i] << " ";
+    // c_list[0] = 99; // illegal
 
 }
