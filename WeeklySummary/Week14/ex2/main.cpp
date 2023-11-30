a0b4b91ded7e29054015bd4e00aec030e84f8815
Dilshod <137488386+dilshod-obidov@users.noreply.github.com>
2023-11-30 14:17:11 +0000

    Week14 ex2

--- WeeklySummary/Week14/ex2/main.cpp
+++ WeeklySummary/Week14/ex2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -9,23 +10,33 @@ private:
 
 public:
     InhaString() : m_msg{nullptr}, m_len{0} {} 
+    InhaString(const char* msg){
+        m_len = strlen(msg);
+        m_msg = new char[m_len];
+        strcpy(m_msg, msg);
+    }
     InhaString(const InhaString& istr) 
-        : m_msg{istr.m_msg}, m_len{istr.m_len} {}
+        : m_len{istr.m_len} {
+            m_msg = new char[istr.m_len];
+            strcpy(m_msg, istr.m_msg);
+        }
     InhaString(const char* msg, const int len)
         : m_len{len} {
             m_msg = new char[len];
-            for(int i = 0; i < len; ++i){
-                m_msg[i] = msg[i];
-            }
+            strcpy(m_msg, msg);
         }
     ~InhaString(){
         if (m_msg) delete m_msg;
     }   
 
     InhaString operator+(const InhaString& istr);
-    
+    InhaString& operator=(const InhaString& istr);
+
+    InhaString& operator+=(const InhaString& istr);
+
     friend ostream& operator<<(ostream& out, const InhaString& istr);
     friend istream& operator>>(istream& in, InhaString& istr);
+    friend bool operator==(const InhaString& istr1, const InhaString& istr2);
 };
 
 InhaString InhaString::operator+(const InhaString& istr){
@@ -41,16 +52,45 @@ InhaString InhaString::operator+(const InhaString& istr){
     delete[] temp_msg;
     return temp_istr;
 }
+InhaString& InhaString::operator=(const InhaString& istr){
+    m_msg = istr.m_msg;
+    m_len = istr.m_len;
+    return *this;
+}
 
+InhaString& InhaString::operator+=(const InhaString& istr){
+    int temp_len = m_len + istr.m_len;
+    char* temp_msg = new char[temp_len];
 
+    for(int i = 0; i < m_len; ++i)
+        temp_msg[i] = m_msg[i];
+    for(int i = 0; i < istr.m_len; ++i)
+        temp_msg[i + m_len] = istr.m_msg[i];
+
+    delete[] m_msg;
+    m_msg = temp_msg;
+    m_len = temp_len;
+    return *this;
+}
 
 ostream& operator<<(ostream& out, const InhaString& istr){
     out << istr.m_msg;
     return out;
 }
 istream& operator>>(istream& in, InhaString& istr){
-    in >> istr.m_len;
+    char temp[100];
+    in >> temp;
+    delete[] istr.m_msg;
+    istr.m_len = strlen(temp);
+    istr.m_msg = new char[istr.m_len + 1];
+    strcpy(istr.m_msg, temp);
     return in;
+    
+}
+
+bool operator==(const InhaString& istr1, const InhaString& istr2){
+    return (strcmp(istr1.m_msg, istr2.m_msg) == 0 && istr1.m_len == istr2.m_len);
+
 }
 
 int main(){
